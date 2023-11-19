b48c2fba669542690025b695a70f6b4a40a72b7b
Joey <gibbonsjoey95@gmail.com>
2023-11-18 21:35:25 -0600

    edited textarea size

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const buttons = document.querySelectorAll("button")
 const outputField = document.getElementById("outputField")
 const outputFieldResult = document.getElementById("outputFieldResult")
+const equalButton = document.getElementById("equalButton")
 
 let output = ''
 let outputResult = ''
+
+equalButton.addEventListener('click', function(){
+    output = outputResult
+    outputResult = ''
+})
 
 buttons.forEach((button) => {
     button.addEventListener('click', function(){
