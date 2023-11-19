1c72976de655489542dd4b2f5fa7504d939a1df1
Joey <gibbonsjoey95@gmail.com>
2023-11-18 22:34:44 -0600

    got clear button working

--- app.js
+++ app.js
@@ -2,27 +2,28 @@ const operator_button = document.querySelectorAll(".operator-button")
 const outputField = document.getElementById("outputField")
 const outputFieldResult = document.getElementById("outputFieldResult")
 const equalButton = document.getElementById("equalButton")
+const clearButton = document.getElementById("clearButton")
 
 let output = ''
 
 equalButton.addEventListener('click', function(){
-    console.log('output', output)
-
     outputField.value = outputFieldResult.value
     output = outputField.value
-
     outputFieldResult.value = ''
-
-    console.log('output2', output)
 })
 
+clearButton.addEventListener('click', function(){
+    output = ''
+    outputField.value = output
+    outputFieldResult.value = output
+})
 
 operator_button.forEach((button) => {
     button.addEventListener('click', function(){
         output += this.textContent
         outputField.value = output
 
-        if(!isNaN(parseInt(output[0])) && !isNaN(parseInt(output.slice(-1)))){
+        if(!isNaN(parseInt(output[0])) && !isNaN(parseInt(output.slice(-1))) || !isNaN(parseInt(output[0])) && output.slice(-1) === '%'){
             outputFieldResult.value = eval(output)
         }
     })
